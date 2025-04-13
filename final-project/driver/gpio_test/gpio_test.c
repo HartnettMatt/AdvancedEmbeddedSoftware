1c7df1a8544bbeb1567fa61f3d3ebcab263708f8
Matt Hartnett <matthew.e.hartnett@gmail.com>
2025-04-13 16:37:21 -0600

    gpio test

--- final-project/driver/gpio_test/gpio_test.c
+++ final-project/driver/gpio_test/gpio_test.c
@@ -1,41 +1,46 @@
+// Ensure the API version is set before any headers are processed:
 #define GPIOD_API_VERSION 2
 #include <gpiod.h>
-#include <unistd.h>
 #include <stdio.h>
+#include <unistd.h>
 
 #define GPIO_CHIP "/dev/gpiochip0"
-#define GPIO_LINE 17  // GPIO17
+#define GPIO_LINE_OFFSET 17  // Using offset 17
 
-int main() {
+int main(void) {
     struct gpiod_chip *chip;
     struct gpiod_line *line;
-    int val = 0, ret;
+    struct gpiod_line_request_config config = {
+        .consumer = "gpio_test",
+        .request_type = GPIOD_LINE_REQUEST_DIRECTION_OUTPUT,
+        .flags = 0
+    };
+    int ret, val = 0;
 
     chip = gpiod_chip_open(GPIO_CHIP);
     if (!chip) {
-        perror("Open chip failed");
+        perror("Failed to open chip");
         return 1;
     }
 
-    line = gpiod_chip_get_line(chip, GPIO_LINE);
+    line = gpiod_chip_get_line(chip, GPIO_LINE_OFFSET);
     if (!line) {
-        perror("Get line failed");
+        perror("Failed to get line");
         gpiod_chip_close(chip);
         return 1;
     }
 
-    // Use the version 2 API function with explicit flags.
-    ret = gpiod_line_request_output_flags(line, "gpio_test", 0, 0);
+    ret = gpiod_line_request(line, &config, 0);
     if (ret < 0) {
-        perror("Request line as output failed");
+        perror("Failed to request line as output");
         gpiod_chip_close(chip);
         return 1;
     }
 
     while (1) {
-        ret = gpiod_line_set_value(line, val);
+        ret = gpiod_line_request_set_value(line, val);
         if (ret < 0) {
-            perror("Set line value failed");
+            perror("Failed to set line value");
             break;
         }
         printf("GPIO set to %d\n", val);
@@ -43,7 +48,7 @@ int main() {
         sleep(1);
     }
 
-    gpiod_line_release(line);
+    gpiod_line_request_release(line);
     gpiod_chip_close(chip);
     return 0;
 }
