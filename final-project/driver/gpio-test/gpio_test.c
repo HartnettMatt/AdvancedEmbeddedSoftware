295096c95613b5a67c835b971607956c3e3920cb
Matt Hartnett <matthew.e.hartnett@gmail.com>
2025-04-13 14:58:58 -0600

    First attempt at GPIO test driver

---
+++ final-project/driver/gpio-test/gpio_test.c
@@ -0,0 +1,42 @@
+#include <gpiod.h>
+#include <unistd.h>
+#include <stdio.h>
+
+#define GPIO_CHIP "/dev/gpiochip0"
+#define GPIO_LINE 17  // GPIO17
+
+int main() {
+    struct gpiod_chip *chip;
+    struct gpiod_line *line;
+    int val = 0;
+
+    chip = gpiod_chip_open(GPIO_CHIP);
+    if (!chip) {
+        perror("Open chip failed");
+        return 1;
+    }
+
+    line = gpiod_chip_get_line(chip, GPIO_LINE);
+    if (!line) {
+        perror("Get line failed");
+        gpiod_chip_close(chip);
+        return 1;
+    }
+
+    if (gpiod_line_request_output(line, "gpio_test", 0) < 0) {
+        perror("Request line as output failed");
+        gpiod_chip_close(chip);
+        return 1;
+    }
+
+    while (1) {
+        gpiod_line_set_value(line, val);
+        printf("GPIO set to %d\n", val);
+        val = !val;
+        sleep(1);
+    }
+
+    gpiod_line_release(line);
+    gpiod_chip_close(chip);
+    return 0;
+}
