23aac01128bdaf5e4873e4c620872521f139da88
Matt Hartnett <matthew.e.hartnett@gmail.com>
2025-04-13 17:30:57 -0600

    gpio test 4

--- final-project/driver/gpio_test/gpio_test.c
+++ final-project/driver/gpio_test/gpio_test.c
@@ -8,11 +8,10 @@
 
 int main(void) {
     struct gpiod_chip *chip;
-    struct gpiod_line_bulk bulk;
-    int offsets[1] = { GPIO_LINE_OFFSET };
+    struct gpiod_line_request *req;
     int ret;
     int current = 0;
-    int val_array[1];
+    unsigned int offset = GPIO_LINE_OFFSET;
 
     chip = gpiod_chip_open(GPIO_CHIP);
     if (!chip) {
@@ -20,34 +19,27 @@ int main(void) {
         return 1;
     }
 
-    ret = gpiod_chip_get_lines(chip, offsets, 1, &bulk);
-    if (ret < 0) {
-        perror("Failed to get line bulk");
-        gpiod_chip_close(chip);
-        return 1;
-    }
-
-    ret = gpiod_line_request_bulk_output(&bulk, "gpio_test", &current);
-    if (ret < 0) {
-        perror("Failed to request bulk output");
+    req = gpiod_chip_request_lines(chip, "gpio_test",
+                                   GPIOD_LINE_REQUEST_DIRECTION_OUTPUT,
+                                   &offset, 1, &current);
+    if (!req) {
+        perror("Failed to request line");
         gpiod_chip_close(chip);
         return 1;
     }
 
     while (1) {
         current = !current;
-        val_array[0] = current;
-        ret = gpiod_line_set_value_bulk(&bulk, val_array);
+        ret = gpiod_line_request_set_value(req, 0, current);
         if (ret < 0) {
-            perror("Failed to set line bulk value");
+            perror("Failed to set line value");
             break;
         }
         printf("GPIO set to %d\n", current);
         sleep(1);
     }
 
-    // Release the lines and close the chip.
-    gpiod_line_release_bulk(&bulk);
+    gpiod_line_request_release(req);
     gpiod_chip_close(chip);
     return 0;
 }
