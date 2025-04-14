78cf399591893ca9f10cb0807dbffbbddadc9761
Matt Hartnett <matthew.e.hartnett@gmail.com>
2025-04-13 19:55:50 -0600

    GPIO test 6

--- final-project/driver/gpio_test/gpio_test.c
+++ final-project/driver/gpio_test/gpio_test.c
@@ -9,17 +9,50 @@
 int main(void) {
     struct gpiod_chip *chip;
     struct gpiod_line_request *req;
+    struct gpiod_request_config *req_config;
+    struct gpiod_line_config *line_config;
+    struct gpiod_line_settings *line_settings;
     int ret;
     int current = 0;  // initial output value
     unsigned int offset = GPIO_LINE_OFFSET; // single line offset
 
+    // Init chip, request, line config, and line settings
     chip = gpiod_chip_open(GPIO_CHIP);
     if (!chip) {
-        perror("Failed to open chip");
+        perror("Failed to init chip");
         return 1;
     }
 
-    req = gpiod_chip_request_lines(chip, "gpio_test", &offset, 1, current);
+    req_config = gpiod_request_config_new();
+    if (!req_config) {
+        perror("Failed to init request");
+        return 1;
+    }
+
+    line_config = gpiod_line_config_new();
+    if (!line_config) {
+        perror("Failed to init line");
+        return 1;
+    }
+
+    line_settings = gpiod_line_settings_new();
+    if (!line_settings) {
+        perror("Failed to init line settings");
+        return 1;
+    }
+
+    // Configure line settings
+    if(!gpiod_line_settings_set_direction(line_settings, GPIOD_LINE_DIRECTION_OUTPUT)){
+        perror("Failed to set line output direction");
+        return 1;
+    }
+    
+    if(!gpiod_line_config_add_line_settings(line_config, &offsets, 1, line_settings)){
+        perror("Failed to configure line settings");
+        return 1;
+    }
+
+    req = gpiod_chip_request_lines(chip, req_config, line_config);
     if (!req) {
         perror("Failed to request line");
         gpiod_chip_close(chip);
@@ -28,7 +61,7 @@ int main(void) {
 
     while (1) {
         current = !current;
-        ret = gpiod_line_request_set_value(req, 0, current);
+        ret = gpiod_line_request_set_value(req, offset, current);
         if (ret < 0) {
             perror("Failed to set line value");
             break;
@@ -38,7 +71,10 @@ int main(void) {
     }
 
     // Release the line request and close the chip.
+    gpiod_line_settings_free(line_settings);
     gpiod_line_request_release(req);
+    gpiod_line_config_free(line_config)
+    gpiod_request_config_free(req_config);
     gpiod_chip_close(chip);
     return 0;
 }
